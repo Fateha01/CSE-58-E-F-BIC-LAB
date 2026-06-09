9b2fe77103e39af2019f1743df4d967ac23239a1
Fateha Jannat Sumaya <150549532+Fateha01@users.noreply.github.com>
2026-06-09 16:03:45 +0600

    Update Problem3.py

--- Task 2/Problem3.py
+++ Task 2/Problem3.py
@@ -1,1 +1,7 @@
-
+def rev_complt(pattern):
+    mapping ={'A':'T','T':'A','C':'G','G':'C'}
+    result= ""
+    for char in pattern:
+        result =mapping[char] + result
+        return result
+        print(rev_complt("AAAACCCGGT"))
