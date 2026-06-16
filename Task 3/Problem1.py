98f5e9c1cb3ffd28b4bdcb8765b4957858d191d7
Fateha Jannat Sumaya <150549532+Fateha01@users.noreply.github.com>
2026-06-16 16:24:37 +0600

    Update Problem1.py

--- Task 3/Problem1.py
+++ Task 3/Problem1.py
@@ -1,1 +1,18 @@
 
+from itertools import product
+
+def solve(text, k, d):
+    best = {}
+
+    for pattern in [''.join(p) for p in product('ACGT', repeat=k)]:
+        count = 0
+        for i in range(len(text) - k + 1):
+            mismatches = sum(a != b for a, b in zip(text[i:i+k], pattern))
+            if mismatches <= d:
+                count += 1
+        best[pattern] = count
+
+    max_count = max(best.values())      
+    print(' '.join(p for p, c in best.items() if c == max_count))
+
+solve("ACGTTGCATGTCGCATGATGCATGAGAGCT", 4, 1)
